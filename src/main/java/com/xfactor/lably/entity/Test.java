563058eb533adba670c7cd862ef76861dac86ca8
Vijay <vjdubey318@gmail.com>
2021-05-21 01:23:13 +0530

    created entity Test

---
+++ src/main/java/com/xfactor/lably/entity/Test.java
@@ -0,0 +1,47 @@
+package com.xfactor.lably.entity;
+
+public class Test {
+
+    private String Id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+    
+
+}
